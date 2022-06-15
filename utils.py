098a9c3cf2da52a85679275dd56965655d14ca22
felixbmuller <57685553+felixbmuller@users.noreply.github.com>
2022-06-15 13:49:55 +0200

    feat(utils): add timestamp()

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 import torch
+import datetime
 
 
 def disc_l2_loss(disc_value):
@@ -14,3 +15,6 @@ def adv_disc_l2_loss(real_disc_value, fake_disc_value):
     kb = fake_disc_value.shape[0]
     lb, la = torch.sum(fake_disc_value ** 2) / kb, torch.sum((real_disc_value - 1) ** 2) / ka
     return la, lb, la + lb
+
+def timestamp():
+    return datetime.datetime.today().strftime('%y%m%d-%H%M')
