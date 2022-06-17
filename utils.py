c51a69f3e24c5b4d59b04347569a9f98afb67a8b
felixbmuller <57685553+felixbmuller@users.noreply.github.com>
2022-06-17 10:29:51 +0200

    utils: add constants and converter funcs

--- utils.py
+++ utils.py
@@ -1,7 +1,14 @@
-import numpy as np
-import torch
 import datetime
 
+import numpy as np
+import torch
+
+
+N_JOINTS = 15
+BODY_EDGES = np.array(
+[[0,1], [1,2],[2,3],[0,4],
+[4,5],[5,6],[0,7],[7,8],[7,9],[9,10],[10,11],[7,12],[12,13],[13,14]]
+)
 
 def disc_l2_loss(disc_value):
     
@@ -18,3 +25,41 @@ def adv_disc_l2_loss(real_disc_value, fake_disc_value):
 
 def timestamp():
     return datetime.datetime.today().strftime('%y%m%d-%H%M')
+
+def to_np(*tensors):
+    """convert one or multiple torch.Tensor's to np.ndarray's. Does nothing if input is already
+    a numpy array.
+    """
+
+    results = [
+        (t.results.detach().cpu().numpy() if not isinstance(t, np.ndarray) else t)
+        for t in tensors
+    ]
+
+    if len(results) == 1:
+        return results[0]
+    else:
+        return results
+
+
+def to_t(*arrays, dtype=torch.float32, device="cpu"):
+    """convert one or multiple numpy arrays to torch tensors. Does nothing if input is already
+    a tensor.
+
+    Parameters
+    ----------
+    dtype : _type_, optional
+        torch dtype, by default torch.float32
+    device : str, optional
+        torch device, by default "cpu"
+    """
+
+    results = [
+        (torch.tensor(a, dtype=dtype).to(device) if not isinstance(a, torch.Tensor) else a)
+        for a in arrays
+    ]
+
+    if len(results) == 1:
+        return results[0]
+    else:
+        return results
